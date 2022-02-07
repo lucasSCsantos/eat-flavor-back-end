af30366b853bb1acadef0e19aaaea958469e2d50
lucasSCsantos <luc.cristovam10@gmail.com>
2022-02-07 01:32:14 -0300

    :mute: remove log
    remove log on connection mock

--- src/tests/mocks/getConnectionMock.ts
+++ src/tests/mocks/getConnectionMock.ts
@@ -5,8 +5,6 @@ import { MongoMemoryServer } from 'mongodb-memory-server';
 const getConnection = async () => {
   const DBSERVER = await MongoMemoryServer.create();
   const URLMock = DBSERVER.getUri();
-  console.log(URLMock);
-  
   return await MongoClient.connect(URLMock);
 };
 
